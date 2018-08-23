dbdb5b1710dbaa779bcebc959d15ebab1472e884
Eduardo <soportezcrome@gmail.com>
2018-08-23 15:24:44 -0500

    Agregar listado de notes

--- server/boot/root.js
+++ server/boot/root.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
   var ToDo = app.models.ToDo;
   var ToDoUser = app.models.ToDoUser;
   var Token = app.models.AccessToken;
+  var Note = app.models.Note;
 
   function okResponseToClient(responseToClient) {
     responseToClient.code = 200;
@@ -70,6 +71,57 @@ module.exports = function(app) {
       }
     });
   });
+
+
+  router.post('/api/user-notes', function (req, res) {
+    // accessToken string   params
+    // userId number        params
+    //local vars
+		var responseToClient = {
+			code: 500,
+			todos: []
+    };
+
+    if (!req.body.accessToken) {
+			serverFailResponseToClient(responseToClient);
+			return res.json(responseToClient);
+    }
+    
+    if (!req.body.userId) {
+			serverFailResponseToClient(responseToClient);
+			return res.json(responseToClient);
+		}
+    
+    Token.findById(req.body.accessToken, function (err, token) {
+      if (err) {
+        serverFailResponseToClient(responseToClient);
+        return res.json(responseToClient);
+      }
+
+      if (token) {
+        var query = {
+          where: {
+            toDoUserId: token.toDoUserId
+          }
+        };
+
+        Note.find(query, function (err, notes) {
+          if (err) {
+						serverFailResponseToClient(responseToClient);
+						return res.json(responseToClient);
+          }
+          responseToClient.notes = notes
+          okResponseToClient(responseToClient);
+					return res.json(responseToClient);
+        });
+      }else{
+        serverFailResponseToClient(responseToClient);
+				return res.json(responseToClient);
+      }
+    });
+  });
+
+
 
 
 
